845b443d87dca4ab2a6dcdfdf028eb20b1a0e2b2
bradberger01 <bradberger@hotmail.com>
2017-11-01 18:55:08 -0400

    Finally built models and controllers

---
+++ FortuneTellerMVC/Controllers/CustomersController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FortuneTellerMVC.Models;
+
+namespace FortuneTellerMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private FortuneTellerMVCEntities16 db = new FortuneTellerMVCEntities16();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = db.Customers.Include(c => c.Age).Include(c => c.BirthMonth).Include(c => c.FavoriteColor).Include(c => c.NumberOfSibling);
+            return View(customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            ViewBag.AgeID = new SelectList(db.Ages, "AgeID", "AgeID");
+            ViewBag.BirthMonthID = new SelectList(db.BirthMonths, "BirthMonthID", "BirthMonth1");
+            ViewBag.FavoriteColorID = new SelectList(db.FavoriteColors, "FavoriteColorID", "FavoriteColor1");
+            ViewBag.NumberOfSiblingsID = new SelectList(db.NumberOfSiblings, "NumberOfSiblingsID", "NumberOfSiblings");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerID,FirstName,LastName,AgeID,BirthMonthID,FavoriteColorID,NumberOfSiblingsID")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AgeID = new SelectList(db.Ages, "AgeID", "AgeID", customer.AgeID);
+            ViewBag.BirthMonthID = new SelectList(db.BirthMonths, "BirthMonthID", "BirthMonth1", customer.BirthMonthID);
+            ViewBag.FavoriteColorID = new SelectList(db.FavoriteColors, "FavoriteColorID", "FavoriteColor1", customer.FavoriteColorID);
+            ViewBag.NumberOfSiblingsID = new SelectList(db.NumberOfSiblings, "NumberOfSiblingsID", "NumberOfSiblings", customer.NumberOfSiblingsID);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AgeID = new SelectList(db.Ages, "AgeID", "AgeID", customer.AgeID);
+            ViewBag.BirthMonthID = new SelectList(db.BirthMonths, "BirthMonthID", "BirthMonth1", customer.BirthMonthID);
+            ViewBag.FavoriteColorID = new SelectList(db.FavoriteColors, "FavoriteColorID", "FavoriteColor1", customer.FavoriteColorID);
+            ViewBag.NumberOfSiblingsID = new SelectList(db.NumberOfSiblings, "NumberOfSiblingsID", "NumberOfSiblings", customer.NumberOfSiblingsID);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerID,FirstName,LastName,AgeID,BirthMonthID,FavoriteColorID,NumberOfSiblingsID")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AgeID = new SelectList(db.Ages, "AgeID", "AgeID", customer.AgeID);
+            ViewBag.BirthMonthID = new SelectList(db.BirthMonths, "BirthMonthID", "BirthMonth1", customer.BirthMonthID);
+            ViewBag.FavoriteColorID = new SelectList(db.FavoriteColors, "FavoriteColorID", "FavoriteColor1", customer.FavoriteColorID);
+            ViewBag.NumberOfSiblingsID = new SelectList(db.NumberOfSiblings, "NumberOfSiblingsID", "NumberOfSiblings", customer.NumberOfSiblingsID);
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FortuneTellerMVC/Controllers/NumberOfSiblingsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FortuneTellerMVC.Models;
+
+namespace FortuneTellerMVC.Controllers
+{
+    public class NumberOfSiblingsController : Controller
+    {
+        private FortuneTellerMVCEntities16 db = new FortuneTellerMVCEntities16();
+
+        // GET: NumberOfSiblings
+        public ActionResult Index()
+        {
+            return View(db.NumberOfSiblings.ToList());
+        }
+
+        // GET: NumberOfSiblings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NumberOfSibling numberOfSibling = db.NumberOfSiblings.Find(id);
+            if (numberOfSibling == null)
+            {
+                return HttpNotFound();
+            }
+            return View(numberOfSibling);
+        }
+
+        // GET: NumberOfSiblings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NumberOfSiblings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NumberOfSiblingsID,NumberOfSiblings")] NumberOfSibling numberOfSibling)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NumberOfSiblings.Add(numberOfSibling);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(numberOfSibling);
+        }
+
+        // GET: NumberOfSiblings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NumberOfSibling numberOfSibling = db.NumberOfSiblings.Find(id);
+            if (numberOfSibling == null)
+            {
+                return HttpNotFound();
+            }
+            return View(numberOfSibling);
+        }
+
+        // POST: NumberOfSiblings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NumberOfSiblingsID,NumberOfSiblings")] NumberOfSibling numberOfSibling)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(numberOfSibling).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(numberOfSibling);
+        }
+
+        // GET: NumberOfSiblings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NumberOfSibling numberOfSibling = db.NumberOfSiblings.Find(id);
+            if (numberOfSibling == null)
+            {
+                return HttpNotFound();
+            }
+            return View(numberOfSibling);
+        }
+
+        // POST: NumberOfSiblings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            NumberOfSibling numberOfSibling = db.NumberOfSiblings.Find(id);
+            db.NumberOfSiblings.Remove(numberOfSibling);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
